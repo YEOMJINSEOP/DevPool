50af7a745858d205b885b05cf499033570a354be
Young JIn <leesu0229@naver.com>
2023-06-08 18:22:54 +0900

    modify  chat

--- client/src/components/user/UserInfo/Chat.jsx
+++ client/src/components/user/UserInfo/Chat.jsx
@@ -1,46 +1,64 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { BASE_URL, getMemberId } from './LogIn/LogIn'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { BASE_URL, getMemberId } from './LogIn/LogIn';
 import styles from './Chat.module.css';
 
 export default function Chat() {
-  
-  const [chatLog, setChatLog] = useState();
-  const [memberNickName, setMemberNickName] = useState();
+  const [chatLog, setChatLog] = useState([]);
+  const [memberNickName, setMemberNickName] = useState('');
+  const [selectedChat, setSelectedChat] = useState([]);
 
   useEffect(() => {
-    const id = getMemberId().memberId
-    // 추후 1 들어간 부분에 id 가져오기
-    axios.get(`${BASE_URL}/api/latter/1`).then((res) => {
+    const memberId = getMemberId().memberId;
+
+    axios.get(`${BASE_URL}/api/latter/${memberId}`).then((res) => {
       console.log(res.data.dataList);
       setChatLog(res.data.dataList);
-    })
-    axios.get(`${BASE_URL}/api/member/1`).then((res) => {
-      console.log(res.data.data.nickName);
-      setMemberNickName(res.data.data.nickName);
-    })
-  }, [])
+    });
+
+    axios.get(`${BASE_URL}/api/member/${memberId}`).then((res) => {
+      console.log(res.data);
+      setMemberNickName(res.data.nickName);
+    });
+  }, []);
+
+  const handleChatClick = (chatGroup) => {
+    setSelectedChat(chatGroup);
+  };
 
   return (
     <div>
-      {chatLog && chatLog.map((chat, idx) => {
-        return (
-          <div style={{margin: '40px'}} className={styles.chatwrapper} key={idx}>
-            {chat.map((item, idx) => {
-              if(memberNickName == item.senderNickName) {
-                return (
-                  <div key={idx} className={styles.myChat}>{item.content}</div>
-                )
-              }
-              else {
-                return (
-                  <div key={idx} className={styles.otherChat}>{item.content}</div>
-                )
-              }
-            })}
-          </div>
-        )
-      })}
+      {chatLog.map((chatGroup, idx) => (
+        <div
+          style={{ margin: '40px' }}
+          className={styles.chatwrapper}
+          key={idx}
+          onClick={() => handleChatClick(chatGroup)}
+        >
+          {chatGroup.map((chat, idx) => (
+            <div
+              key={idx}
+              className={chat.senderNickName === memberNickName ? styles.myChat : styles.otherChat}
+            >
+              {chat.content}
+            </div>
+          ))}
+        </div>
+      ))}
+
+      {selectedChat.length > 0 && (
+        <div>
+          <h2>Selected Chat</h2>
+          {selectedChat.map((chat, idx) => (
+            <div
+              key={idx}
+              className={chat.senderNickName === memberNickName ? styles.myChat : styles.otherChat}
+            >
+              {chat.content}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
